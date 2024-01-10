fc7e609c7e5f662be01c22244a66968ca17658ea
Anjali Singhal <92749087+anjali-2904@users.noreply.github.com>
2024-01-10 23:12:01 +0530

    Create min_max_Element.java

---
+++ Array/min_max_Element.java
@@ -0,0 +1,13 @@
+class Solution{
+public static int findSum(int A[],int N){
+        //code here
+        int max=A[0],min=A[0];
+        for(int i=0;i<N;i++){
+            if(A[i]<min)
+            min=A[i];
+            if(A[i]>max)
+            max=A[i];
+        }
+        return max+min;
+    }
+}
